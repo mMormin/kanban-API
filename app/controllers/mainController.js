f499418ed2c4acc6371955b063cda8bdff9ae3b0
mMormin <mmboudot@gmail.com>
2023-09-26 15:50:54 +0200

    :heavy_minus_sign: Ejs dep removal

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -24,7 +24,7 @@ const mainController = {
   },
 
   errorPage: (req, res) => {
-    res.status(404).render("status", { status: "404" });
+    res.status(404).render("Cette ressourcec n'existe pas");
   },
 };
 
