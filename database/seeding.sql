9ed2a02aaf9e7550be68a2898a8d097608f2a516
mMormin <mmboudot@gmail.com>
2023-09-27 14:16:31 +0200

    :poop: Seeding file

--- database/seeding.sql
+++ database/seeding.sql
@@ -1,10 +1,28 @@
 BEGIN;
 
-INSERT INTO "member" ("id", "email", "password", "role") VALUES (1, 'admin@admin.io', 'salut', 'admin');
-INSERT INTO "board" ("id", "title", "description", "member_id") VALUES (1, 'Perso', 'J''ai faim !!', 1);
-INSERT INTO "card" ("id", "title", "position", "board_id") VALUES (1, 'Liste des courses', 0, 1);
-INSERT INTO "todo" ("id", "title", "position", "card_id") VALUES (1, '5 pommes', 0, 1), (2, '1kg de PDT', 1, 1), (3, '3 Melons', 2, 1), (4, 'Pain', 3, 1), (5, 'Pack d''Iced Tea', 4, 1);
-INSERT INTO "tag" ("id", "name") VALUES (1, 'Urgent'), (2, 'Caprice'), (3, 'Cadeau');
-INSERT INTO "todo_has_tag" ("todo_id", "tag_id") VALUES (5, 1), (4, 2) ,(3, 3), (3, 2);
+TRUNCATE TABLE "member", "board", "card", "todo", "tag", "todo_has_tag" RESTART IDENTITY CASCADE;
+
+INSERT INTO "member" ("email", "password", "role")
+VALUES ('admin@admin.io', 'salut', 'admin');
+
+INSERT INTO "board" ("title", "description", "member_id")
+VALUES ('Perso', 'J''ai faim !!', 1);
+
+INSERT INTO "card" ("title", "position", "board_id")
+VALUES ('Liste des courses', 0, 1);
+
+INSERT INTO "todo" ("title", "position", "card_id")
+VALUES
+('5 pommes', 0, 1),
+('1kg de PDT', 1, 1),
+('3 Melons', 2, 1),
+('Pain', 3, 1),
+('Pack d''Iced Tea', 4, 1);
+
+INSERT INTO "tag" ("name")
+VALUES ('Urgent'), ('Caprice'), ('Cadeau');
+
+INSERT INTO "todo_has_tag" ("todo_id", "tag_id")
+VALUES (5, 1), (4, 2) ,(3, 3), (3, 2);
 
 COMMIT;
